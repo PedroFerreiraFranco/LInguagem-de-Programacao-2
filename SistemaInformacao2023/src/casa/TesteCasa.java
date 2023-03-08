87611a9cc86c8a75f91d65160946263f48be2f0b
Pedro Franco <101286154+PedroFerreiraFranco@users.noreply.github.com>
2023-03-08 07:19:59 -0300

    Update TesteCasa.java

--- SistemaInformacao2023/src/casa/TesteCasa.java
+++ SistemaInformacao2023/src/casa/TesteCasa.java
@@ -10,5 +10,17 @@ package casa;
  * @author Pedro
  */
 public class TesteCasa {
-    
+    public static void main(String[] args) {
+        Casa casa = new Casa();
+        int qtdCasa=0;
+        
+        casa.setCor("rosa");
+        casa.p1.fecha();
+        qtdCasa= casa.quantasPortasEstaoAbertas();
+        System.out.println("Quantidade de portas abertas: "+qtdCasa);
+        System.out.println("Nova cor da casa: "+casa.getCor());
+        casa.mudaCor("azul");
+        System.out.println("Nova cor da casa: "+casa.getCor());
+        
+    }
 }
